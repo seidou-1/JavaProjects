9c650ded4158c0b61547a2d5785febc489779329
MB <mbarry323@gmail.com>
2018-07-20 14:21:55 -0400

    inheritance by way of extends keyword

--- Side Projects/Sandbox Exercises and Examples/Scenarios/src/main/java/JavaVsPython/Classes.java
+++ Side Projects/Sandbox Exercises and Examples/Scenarios/src/main/java/JavaVsPython/Classes.java
@@ -14,12 +14,10 @@ public class Classes {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
 
-//You can have a class within a class
-    public class DivideAndMultiply {
+//You can have a class within a class no problem (nested classes)
+    public class DivideAndMultiply { 
 
         int c;
 
@@ -35,8 +33,39 @@ public class Classes {
         }
     }
 
-    public class AddAndSubtract extends DivideAndMultiply {
+    public static class AddAndSubtract extends DivideAndMultiply { //extends means inherits
+        
+        public void add (int a, int b){
+            c = a + b;
+            System.out.println("Result = " + c);
+        }
+        
+        public void subtract (int a, int b){
+            c = a - b;
+            System.out.println("Result = " + c);
+        }
+        
 
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        AddAndSubtract myMath = new AddAndSubtract();
+        
+        /*
+        Since the AddAndSubtract class inherits from the DivideAndMultiply class
+        (by way of the extends keyword), AddAndSubtract becomes a child of
+        DivideAndMultiply.
+        
+        Now when i instantiate the AddAndSubtract class (line 54), i have access
+        to all the child and parent methods
+        */
+        myMath.add(5, 10);//Child method
+        myMath.subtract(20, 15);//Child method
+        myMath.multiplication(25, 4);//Parent method
+        myMath.division(15, 5);//Parent method
     }
 
 } //End of "Classes" class
+
