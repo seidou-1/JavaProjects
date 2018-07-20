6784452fe88966e7d5a6313097238fff210d59a5
MB <mbarry323@gmail.com>
2018-07-19 23:22:04 -0400

    added conditional break comparison

--- Side Projects/Sandbox Exercises and Examples/Scenarios/src/main/java/JavaVsPython/Loops.java
+++ Side Projects/Sandbox Exercises and Examples/Scenarios/src/main/java/JavaVsPython/Loops.java
@@ -16,7 +16,7 @@ public class Loops {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         /*
         In Python, there's not really the concept of an index counter
         that controls the number of iterations in a for loop i.e. int i = 0;
@@ -28,27 +28,58 @@ public class Loops {
         
         for x in range (0, 10):
             print(x)
-        */
-        
+         */
         //Java version:
         for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+
+        /*
+        Let's say we want to iterate through a collection:
+         */
+        //One way of initializing an array in Java:
+        String[] weekends = new String[]{"Saturday", "Sunday"};
+
+        //Second way of initializing an array in Java:
+        String weekdays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        //Iterating through a collection of things in Java:
+        for (String bucket : weekends) {
+            System.out.println(bucket);
+        }
+
+        /*
+        In Python it would look like this:
+        
+        weekdays = ["Monday","Tuesday","Wednesday", "Thursday", "Friday"]
+        
+        for d in weekdays:
+            print (d)
+        
+         */
+        
+        /*
+        What if we wanted to break out of a loop following a condition?
+        In Java it would look like this:
+        */
+        
+        for (int i = 0; i < 10; i++) {
+            
+            if (i == 5) {
+                break;
+            }
             System.out.println(i);
             
         }
         
         /*
-        Let's say we want to iterate through a collection:
+        In Python, it would look like this:
+        
+        for x in range(0, 10):
+            print (x)
+            if (x==5): break
         */
-        
-        //One way of initializing an array in Java:
-        String[] weekends = new String[] {"Saturday", "Sunday"};
-        
-        //Second way of initializing an array in Java:
-        String weekdays[] = {"Monday","Tuesday","Wednesday", "Thursday", "Friday"};
-        
-        for (String bucket : weekends){
-            System.out.println(bucket);
-        }
     }
-    
+
 }
